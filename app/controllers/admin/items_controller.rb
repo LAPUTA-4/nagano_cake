2f0a739d67a7eb4c2d8b24d0695e9d65357173d2
otinu <fitshter@gmail.com>
2021-05-18 22:57:08 +0900

    add-admin/item-create

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,12 +1,27 @@
 class Admin::ItemsController < ApplicationController
+  protect_from_forgery #createアクションのsaveメソッドがRailsのCSRF対策のために通らなくなる。 追記するとなぜか通る？
+  
   def index
   end
 
   def new
+    @item = Item.new
+    @genres = Genre.all
   end
 
   def create
+    if Item.create(item_params)
+      flash[:notice] = "successfully!"
+      redirect_to root_path
+    else
+      flash[:notice] = "can't be blank"
+      redirect_to admin_items_path
+    end
   end
+  
+  
+  
+  
 
   def show
   end
@@ -16,4 +31,11 @@ class Admin::ItemsController < ApplicationController
 
   def update
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name, :introduction, :genre_id, :price, :is_active)
+  end
+  
 end
