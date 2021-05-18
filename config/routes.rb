1817c7eb90dba2ea48a3e8f2143c2178a4daeac1
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-18 13:49:40 +0000

    add customer-edit-view

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,16 @@ Rails.application.routes.draw do
 devise_for :admins, path: 'admin', controllers: {
     sessions: 'admin/sessions'
   }
-devise_for :customers, path:'customers', controllers: {
-    sessions: 'public/sessions',
-    registrations: 'public/registrations'
-}
+#devise_for :customers, path:'customers', controllers: {
+#    sessions: 'public/sessions',
+#    registrations: 'public/registrations'
+#}
+devise_for :customers, skip: :all
+devise_scope :customer do
+  get '/customers/sign_in' => 'public/sessions#new', as: :new_customer_session
+  post '/customers/sign_in' => 'public/sessions#create', as: :customer_session
+  delete '/customers/sign_out' => 'public/sessions#destroy', as: :destroy_customer_session
+end
     #パブリック側(scopemoduleでパブリックフォルダを参照しつつURLは変えない)
 scope module: :public do
     #ホーム
