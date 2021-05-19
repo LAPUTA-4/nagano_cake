eb5fadb2837cae6507a4b72d26c7e2f2c30bc610
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-19 13:37:52 +0000

    fix css

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,11 +6,13 @@ class Public::ItemsController < ApplicationController
       @genre = Genre.find_by(:name => params[:name])
        
       # category_idと紐づく投稿を取得
-      @items = @genre.items.all
+      @items = @genre.items.page(params[:page]).per(8)
+      @items_count = @genre.items.all.count
     else
       # 投稿すべてを取得
-      @items = Item.all
+      @items = Item.page(params[:page]).per(8)
     end
+    @genres = Genre.all
   end
 
   def show
