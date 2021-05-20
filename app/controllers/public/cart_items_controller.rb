dd582e4f8087419bb5216400387208fb8e18476d
otinu <fitshter@gmail.com>
2021-05-20 09:25:01 +0900

    save cart/index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,9 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_item = CartItem.where(customer_id: current_customer.id)
+    @item = Item.where(id: @cart_item.item_id)
+    binding.pry
+    #@item = Item.find_by!(id:@cart_item.item_id)
   end
 
   def update
@@ -12,5 +16,13 @@ class Public::CartItemsController < ApplicationController
   end
 
   def create
+    CartItem.create(item_params)
   end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_id, :amount) 
+  end
+  
 end
