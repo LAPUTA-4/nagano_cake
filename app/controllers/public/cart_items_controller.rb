44036ffb23c2f815201135ba2c15e1810cad7fab
otinu <fitshter@gmail.com>
2021-05-20 15:46:57 +0900

    add cart-index

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,9 +1,11 @@
 class Public::CartItemsController < ApplicationController
   def index
-    @cart_item = CartItem.where(customer_id: current_customer.id)
-    @item = Item.where(id: @cart_item.item_id)
-    binding.pry
-    #@item = Item.find_by!(id:@cart_item.item_id)
+     @customer = Customer.find_by(id: current_customer.id)
+     @cart_items = @customer.cart_items.all
+     @cart_items.each do |n|
+      count = n.item.price * n.amount
+      @sum = count + @sum.to_i
+     end
   end
 
   def update
@@ -13,10 +15,14 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    cart_items = CartItem.where(customer_id: current_customer.id)
+    cart_items.each do |cart_item|
+      cart_item.destroy
+    end
+    redirect_to request.referer
   end
 
   def create
-    CartItem.create(item_params)
     if @cart_item = CartItem.create(cart_item_params)
       redirect_to cart_items_path
     else
@@ -29,4 +35,5 @@ class Public::CartItemsController < ApplicationController
   def cart_item_params
     params.require(:cart_item).permit(:customer_id, :item_id, :amount)
   end
+  
 end
