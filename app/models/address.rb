625e13c7cc973afa1354118e2f48527b75778953
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-24 17:59:23 +0000

    add address-valid

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,10 @@
 class Address < ApplicationRecord
     belongs_to :customer
+    
+    validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
+    validates :address, presence: true
+    validates :name, presence: true
+    
     def view_all #テーブルカラムの連結
       '〒' + self.postal_code + '　' + self.address.to_s + '　' + self.name.to_s
     end
