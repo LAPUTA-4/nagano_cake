a272755f7d99ebe6d252ae8ca1b9dae5f00f710d
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-24 12:08:31 +0000

    fix kaminari-pager

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,11 +6,11 @@ class Public::ItemsController < ApplicationController
       @genre = Genre.find_by(:name => params[:name])
        
       # category_idと紐づく投稿を取得
-      @items = @genre.items.page(params[:page]).per(8)
+      @items = @genre.items.page(params[:page]).per(7)
       @items_count = @genre.items.all.count
     else
       # 投稿すべてを取得
-      @items = Item.page(params[:page]).per(8)
+      @items = Item.page(params[:page]).per(7)
     end
     @genres = Genre.all
   end
