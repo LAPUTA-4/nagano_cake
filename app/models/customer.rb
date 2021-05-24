9d84070b2478ccda32c4b637b6c92279ebbbbb01
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-24 17:55:09 +0000

    fix valid

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,8 +10,8 @@ class Customer < ApplicationRecord
   
   validates :postal_code, presence: true, format: { with: /\A\d{7}\z/ }
   validates :telephone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
-  validates :last_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
-  validates :first_name, presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :last_name, presence: true
+  validates :first_name, presence: true
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :address, presence: true
