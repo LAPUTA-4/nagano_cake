7bba4ce9ad9c194b0d21cb65510d5fcb27c2adf8
UDAZ <u.ka2.sally.cinnamon@gmail.com>
2021-05-26 20:30:48 +0000

    add zoom-js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,7 +39,7 @@ $(document).on('turbolinks:load',function() {
   });
  });
 });
-
+/*================== 商品詳細 ==================*/
 $(document).on('turbolinks:load',function() {
   $(document).ready(function () {
    var gazou = $("#zoom").attr('src');
